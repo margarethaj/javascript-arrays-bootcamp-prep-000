32bd22a5bd37b7d609ba65716dde596d1533b975
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-15 23:24:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
 var chocolateBars = [
   'snickers', 'hundred grand', 'kitkat', 'skittles'
 ]
+
+function addElementToBeginningOfArray(array, element){
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element){
+  return array.push(element)
+}
