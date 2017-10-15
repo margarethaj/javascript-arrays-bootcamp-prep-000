338e9486d7dfc0de1f946e9836815b4beddfad09
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-15 23:42:04 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,16 +6,17 @@ function addElementToBeginningOfArray(array, element){
   var temp = [element, ...array];
   return temp
 }
+
+function addElementToEndOfArray(array, element){
+  var temp = [...array, element];
+  return temp
+}
+
 /*
-
-
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array
 }
 
-function addElementToEndOfArray(array, element){
-  return [..array, element]
-}
 
 function destructivelyAddElementToEndOfArray(array, element){
   return array
