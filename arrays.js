a5315c06c5c0ef60a6e4d1a957bd73c683cde2da
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-16 00:00:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,6 @@ function removeElementFromEndOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  return array.slice(0, array.length - 1)
-  //return array
+  var array = array.slice(0, array.length - 1)
+  return array
 }
